628b45dbaaf043da179c9d72c74b4de86033b8bc
Miltontarrillo <miltontarrillo@gmail.com>
2014-06-26 14:52:19 -0500

    Update Conexion.java

--- PruebaHitHub/src/pe/upeu/config/Conexion.java
+++ PruebaHitHub/src/pe/upeu/config/Conexion.java
@@ -11,5 +11,26 @@ package pe.upeu.config;
  * @author alum.fial8
  */
 public class Conexion {
+    private static Connection conex=null;
+    private static String url="jdbc:mysql://localhost/bd_visitas";
+    private static String usuario="root";
+    private static String clave="root";
+    
+    public static Connection getConexion(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");
+        if(conex=null){
+            conex=DriverManager.getConnection(url,usuario,clave);
+        }
+        else{
+            conex=null;
+        }
+        }
+    }catch(Exception e){
+        JOptionPane.ShowMessage(null, e);
+    }
+    return conex;
+    }
+    }
     
 }
